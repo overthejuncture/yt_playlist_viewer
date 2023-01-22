97a0f9e61f7fb178fe8fbbf602af2dc7cee72067
overthejuncture <overthejuncture@gmail.com>
2023-01-23 01:14:42 +0300

    nested set lib added

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Kalnoy\Nestedset\NodeTrait;
 
 class Category extends Model
 {
     use HasFactory;
+    use NodeTrait;
 
     public $timestamps = false;
 
---
+++ database/migrations/2023_01_22_210204_recreate_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->dropColumn('parent_id');
+        });
+        Schema::table('categories', function (Blueprint $table) {
+            $table->nestedSet();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->dropNestedSet();
+        });
+        Schema::table('categories', function (Blueprint $table) {
+            $table->unsignedBigInteger('parent_id')->nullable();
+        });
+    }
+};
